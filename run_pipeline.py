c3561b4d9ad2c5594158b39e6176e03ba4a3f375
CoreFlow <gamingpoddagames@gmail.com>
2026-03-24 00:34:17 +0530

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,11 @@
 import news_fetcher
 import video_maker
+import shutil
+import os
+
+# Clear old videos to avoid GitHub folder conflict
+if os.path.exists("videos"):
+    shutil.rmtree("videos")
 
 headlines = news_fetcher.get_headlines()
 video_maker.make_videos(headlines)
