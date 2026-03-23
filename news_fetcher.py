1e924514b44b352d7f7a53bf73df8cb2fc1fc0fc
CoreFlow <gamingpoddagames@gmail.com>
2026-03-23 23:58:30 +0530

    Update news_fetcher.py

--- news_fetcher.py
+++ news_fetcher.py
@@ -1,1 +1,15 @@
+import feedparser
 
+RSS_FEEDS = [
+    "http://feeds.bbci.co.uk/news/rss.xml",
+    "http://feeds.reuters.com/reuters/worldNews",
+    "https://www.aljazeera.com/xml/rss/all.xml"
+]
+
+def get_headlines():
+    headlines = []
+    for feed in RSS_FEEDS:
+        parsed = feedparser.parse(feed)
+        for entry in parsed.entries[:3]:  # smaller for GitHub Actions limits
+            headlines.append(entry.title)
+    return headlines
